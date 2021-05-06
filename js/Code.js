8a1f4827865f66deb1a2f87a17d0941df3ff0fd7
GarimaKubde <75133096+GarimaKubde@users.noreply.github.com>
2021-05-06 08:26:08 -0700

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+
+fill("white")
+textSize(15)
+text("U T I N F O N C", 700,150)
+fill("lightblue")
+text("Hint: Does a particular !!", 700,170)
+
+fill("white")
+textSize(15)
+text("T A B A S A E D", 100,250)
+fill("lightblue")
+text("Hint: stores data !!", 100,270)
+}
